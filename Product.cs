6fbf55a0ae481cc3d25e19f32b427da3ce861b0a
Talha Khalifa <Tkhalifa9673@conestogac.on.ca>
2025-02-04 16:56:28 -0800

    Updated Product Class with methods, properties and constructor

--- Product.cs
+++ Product.cs
@@ -8,5 +8,38 @@ namespace Assignment2
 {
     internal class Product
     {
+        public int ProdId { get; set; }
+        public string ProdName { get; set; }  
+        public int ItemPrice { get; set; }
+        public int StockAmount { get; set; }
+
+      
+        public Product(int prodId, string prodName, int itemPrice, int stockAmount)
+        {
+            ProdId = prodId;  
+            ProdName = prodName;  
+            ItemPrice = itemPrice;
+            StockAmount = stockAmount;
+        }
+
+        
+        public void IncreaseStock(int amount)
+        {
+            if (amount > 0)
+            {
+                StockAmount += amount;
+            }
+        }
+
+        
+        public bool DecreaseStock(int amount)
+        {
+            if (amount > 0 && StockAmount >= amount)
+            {
+                StockAmount -= amount;
+                return true;
+            }
+            return false;
+        }
     }
 }
